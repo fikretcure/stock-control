392c1ee9a3981331ea7a6623f5ced0ae6d1bd2a5
Fikret CURE <fikretcure@gmail.com>
2025-02-27 16:09:01 +0300

    updated reset.sh

--- reset.sh
+++ reset.sh
@@ -1,2 +1,3 @@
 docker exec -it workspace bash -c "composer update"
+docker exec -it workspace bash -c "php artisan optimize:clear"
 docker exec -it workspace bash -c "php artisan migrate:fresh --seed"
