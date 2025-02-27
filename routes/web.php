2a2f27231354b75605c2cd2bfab686267cb6325f
Fikret CURE <fikretcure@gmail.com>
2025-02-27 15:07:18 +0300

    updated web.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Welcome Stock Control Api',
+        'deploy' => env('APP_ENV'),
+        'now' => now()->toDateTimeString(),
+    ]);
 });
