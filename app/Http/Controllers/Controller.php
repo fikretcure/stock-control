694b943bab1132a6844ca035cfb2220e9f5def0b
Fikret CURE <fikretcure@gmail.com>
2025-02-28 16:57:28 +0300

    updated user service

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,13 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function success($data = null)
+    {
+        return response()->json($data);
+    }
+
+    public function fail($data = null, $statusCode = 400)
+    {
+        return response()->json($data, $statusCode);
+    }
 }
