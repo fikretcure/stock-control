0640fae0835c43fd06876b96dfd324c4048b2112
Fikret CURE <fikretcure@gmail.com>
2025-03-06 18:51:42 +0300

    Refactor Supplier Controller and Elastic Service
    - Updated SupplierController with improved error handling and resource transformation
    - Removed manual Elasticsearch indexing methods from SupplierElastic service
    - Added type hints and return types to controller methods
    - Simplified exception handling in controller actions
    - Utilized SupplierResource for consistent data response

--- app/Services/Elastic/SupplierElastic.php
+++ app/Services/Elastic/SupplierElastic.php
@@ -4,9 +4,6 @@ namespace App\Services\Elastic;
 
 use App\Models\Supplier;
 use Elastic\Elasticsearch\Exception\AuthenticationException;
-use Elastic\Elasticsearch\Exception\ClientResponseException;
-use Elastic\Elasticsearch\Exception\MissingParameterException;
-use Elastic\Elasticsearch\Exception\ServerResponseException;
 
 /**
  *
@@ -19,47 +16,5 @@ class SupplierElastic extends ElasticService
      */
     public function __construct(){
         parent::__construct(new Supplier()->getTable());
-    }
-
-
-    /**
-     * @param object $supplier
-     * @return array
-     */
-    protected function dataDto(object $supplier): array
-    {
-        return [
-            'id' => $supplier->id,
-            'name' => $supplier->name,
-            'reg_no' => $supplier->reg_no,
-            'created_at' => $supplier->created_at,
-            'deleted_at' => $supplier->deleted_at,
-        ];
-    }
-
-
-    /**
-     * @param object $category
-     * @return void
-     * @throws ClientResponseException
-     * @throws MissingParameterException
-     * @throws ServerResponseException
-     */
-    public function store(object $category): void
-    {
-        parent::storeIndex($this->dataDto($category));
-    }
-
-
-    /**
-     * @param object $category
-     * @return void
-     * @throws ClientResponseException
-     * @throws MissingParameterException
-     * @throws ServerResponseException
-     */
-    public function update(object $category): void
-    {
-        parent::updateIndex($this->dataDto($category));
     }
 }
